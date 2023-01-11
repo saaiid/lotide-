5b4a6e0d8f1fb5b72a05419f4c91a9a5e2693a93
saaiid <ibrosakid@gmail.com>
2023-01-11 09:42:44 -0500

    emplementing essetEqual with emoji

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("lighthouse", "lighthouse");
+assertEqual("Hello", "world");
+assertEqual(20, 20);
+assertEqual(30, 25);
