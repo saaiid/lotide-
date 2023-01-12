f3470356b1eb71c908fde2e82734159fdc4df6d5
saaiid <ibrosakid@gmail.com>
2023-01-12 17:58:15 -0500

    Implement eqArrays Lotide Problem comparing between Arrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1,2], [1]), false);
+
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`PASSED: ${actual} is equal to ${expected}`);
+  } else {
+    console.log(`FAILED: ${actual} is not equal to ${expected}`);
+  }
+}
