4a2fbc8ded3c3d694f58f885d8f0a11a4c2c3772
saaiid <ibrosakid@gmail.com>
2023-01-15 20:33:25 -0500

    Implement findKey

--- findKey.js
+++ findKey.js
@@ -1,3 +1,7 @@
+/* Instruction
+Implement the function findKey which takes in an object and a callback. It should scan the object and return the first 
+key for which the callback returns a truthy value. If no key is found, then it should return undefined. */
+
 function findKey(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
