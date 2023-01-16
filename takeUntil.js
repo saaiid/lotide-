6528178f6acc53605e1e0d0b39e2570108e1e4cf
saaiid <ibrosakid@gmail.com>
2023-01-15 20:11:45 -0500

    Implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,22 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return result;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+// Output: [ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+// Output: [ 'I\'ve', 'been', 'to', 'Hollywood' ]
