91a4037701e9674f36dcf67415b3d2b8abd63b6d
saaiid <ibrosakid@gmail.com>
2023-01-16 23:01:57 -0500

    Incorporate Mocha and Chai

--- head.js
+++ head.js
@@ -6,5 +6,7 @@ const head = function(arr) {
 };
 
 //TEST CASES
-assertEqual((head([5,6,7]), 5));
-assertEqual((head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+//assertEqual((head([5,6,7]), 5));
+//assertEqual((head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+module.exports = head;
